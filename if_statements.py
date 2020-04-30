1a42d880dddaceb08bcbeb4bd7a7ef9b2960b5c2
john-ppd <63131902+john-ppd@users.noreply.github.com>
2020-04-30 00:13:35 -0400

    made pick one script

---
+++ if_statements.py
@@ -0,0 +1,11 @@
+is_male = True
+is_tall = True
+
+if is_male and is_tall:
+    print("You are a tall male")
+elif is_male and not(is_tall):
+    print("You are a short male")
+elif not(is_male) and is_tall:
+    print("You are a tall female")
+elif not(is_male) and not(is_tall):
+    print("You are a short female")
